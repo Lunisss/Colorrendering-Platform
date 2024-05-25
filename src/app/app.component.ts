f27129b3fbed2dd1c7c5edd35bd7fcfa79bf13dd
Jakub Figaj <jakub.figaj@gmail.com>
2024-05-26 01:10:24 +0200

    reworked angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ColorsComponent } from './colors/colors.component';
 import { PictureComponent } from './picture/picture.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ColorsComponent, PictureComponent],
+  imports: [RouterOutlet, ColorsComponent, PictureComponent, NavbarComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
